2ac550dc3efdc0e5331d4f13f1bef8b7f4337c76
attillax <attillax.lab@gmail.com>
2016-05-30 17:44:49 +0200

    Fix empty lines in ex29 add ex30

--- ex29.py
+++ ex29.py
@@ -2,7 +2,6 @@ people = 20
 cats = 30
 dogs = 15
 
-
 if people < cats:
     print "Too many cats! The world is doomed!"
 
@@ -15,7 +14,6 @@ if people < dogs:
 if people > dogs:
     print "The world is dry!"
 
-
 dogs += 5
 
 if people >= dogs:
@@ -23,7 +21,6 @@ if people >= dogs:
 
 if people <= dogs:
     print "People are less than or equal to dogs."
-
 
 if people == dogs:
     print "People are dogs."
